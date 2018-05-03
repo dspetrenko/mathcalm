c47f1b9db58e22cda3a4c89eb6a9093d1ec23127
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-05-03 17:35:31 +0300

    init  Competition model test

--- mathcalm/competition/tests.py
+++ mathcalm/competition/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
+from .models import Competition
+
 # Create your tests here.
+
+
+class CompetitionTestCase(TestCase):
+
+    def test_creation(self):
+
+        import datetime
+
+        competition = Competition()
+        competition.title = 'Math talent competition'
+        competition.text = "It's best competition for young brains. " \
+                           "Best community. Hard tasks."
+
+        competition.start = datetime.datetime.now()
+        duration = 1
+        competition.end = competition.start + datetime.timedelta(days=duration)
+
+        competition.save()
