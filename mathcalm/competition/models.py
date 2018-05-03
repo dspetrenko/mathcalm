871b238c2f329c666605951b0bb9b4ad76466975
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-05-03 17:12:48 +0300

    add Competition and Event models

--- mathcalm/competition/models.py
+++ mathcalm/competition/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Competition(models.Model):
+
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+
+class Event(models.Model):
+
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+    competition = models.ForeignKey(to=Competition, on_delete=models.CASCADE)
