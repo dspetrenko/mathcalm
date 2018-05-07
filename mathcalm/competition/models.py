4c289ecdfee0f5d7d1eb48a3f2603eeb815d9216
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-05-07 17:42:42 +0300

    add organizer to Competiotion model

--- mathcalm/competition/models.py
+++ mathcalm/competition/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Competition(models.Model):
+
+    organizer = models.ForeignKey(to=User, on_delete=models.CASCADE)
 
     title = models.CharField(max_length=250)
     description = models.TextField()
