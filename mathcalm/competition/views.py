d3af3ac3bd29d55d70190073a943f10af4e6054e
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-05-08 16:38:45 +0300

    init view competitions

--- mathcalm/competition/views.py
+++ mathcalm/competition/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
+from django.views.decorators.http import require_GET
 # Create your views here.
+
+
+@require_GET
+def competitions(request):
+
+    return HttpResponse()
