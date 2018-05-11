cd5f6cabe1ec3a4bbff535bebf9b427aed4d9a0b
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-05-11 16:48:16 +0300

    init competitions view

--- mathcalm/competition/views.py
+++ mathcalm/competition/views.py
@@ -1,10 +1,18 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views.decorators.http import require_GET
+
+from .models import Competition
 # Create your views here.
 
 
 @require_GET
 def competitions(request):
 
-    return HttpResponse()
+    comp_list = Competition.objects.all()
+    context = {
+        'competitions': comp_list,
+    }
+
+    return render(request, 'competitions.html', context=context)
